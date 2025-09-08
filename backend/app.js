f126b5858db3a1fd42cd1f20a3b355d0d54e68e7
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-09-08 03:41:35 -0700

    set up the server

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+import express from "express"
+import {Server  } from "socket.io";
+import {createServer, METHODS} from "http"
+import cors from "cors"
+
+
+const app = express();
+
+
+
+const server = createServer(app)
+const io = new Server(server,
+   {
+    cors:{
+      origin:"*",
+     methods: ["GET", "POST"],
+      credentials:true
+    }
+   }
+)
+const PORT = 3000;
+
+// Middleware (optional)
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, Express server!');
+});
+
+io.on("connection",(socket)=>{
+     console.log("connect")
+     console.log("id",socket.id)
+})
+// Start server
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)})
